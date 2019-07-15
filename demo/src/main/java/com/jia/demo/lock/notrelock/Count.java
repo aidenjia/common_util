0573366dba96dafac362907f555ebce20c49f9da
jiazhen <jiazhen@jiangduoduo.com>
2019-07-15 16:59:10 +0800

    lock

--- demo/src/main/java/com/jia/demo/lock/notrelock/Count.java
+++ demo/src/main/java/com/jia/demo/lock/notrelock/Count.java
@@ -1,4 +1,15 @@
 package com.jia.demo.lock.notrelock;
 
 public class Count {
+    Lock lock = new Lock();
+    public void print() throws InterruptedException {
+        lock.lock();
+        doAdd();
+        lock.unlock();
+    }
+    public void doAdd() throws InterruptedException {
+        lock.lock();
+        //do something
+        lock.unlock();
+    }
 }
