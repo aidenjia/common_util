0e3200fc6b182ded7a2f7ca0e9a25d874e7581e8
zhenjacky <jiazhen1021@163.com>
2020-01-28 22:35:41 +0800

    纸牌游戏

--- demo/src/main/java/com/jia/demo/poker/Game.java
+++ demo/src/main/java/com/jia/demo/poker/Game.java
@@ -3,115 +3,84 @@ package com.jia.demo.poker;
 import java.util.*;
 
 public class Game {
-    private PlayWay playWay;
-    private List<Player> playerList;
-    private Set<PokerCard> pokerCardSet;
-    private Integer playNum;
+    private List<Player> playerList = new ArrayList<>();
+    private Set<PokerCard> pokerCardSet = Collections.synchronizedSet(new HashSet<>());
+    private Integer threadCount;
+    private volatile int numResult;
+
 
     public Game() {
-        // playWay = new PlayWay();
-        playerList = new ArrayList<>();
-        pokerCardSet = new HashSet<>();
-        // playNum = 0;
     }
 
-    public void init(Integer playerNum, PlayWay playWay) {
-        this.playNum = playerNum;
-        this.playWay = playWay;
+    public void init(Integer threadCount, Integer reformCardCode) {
+        this.threadCount = threadCount;
         ***REMOVED***
-        for (int i = 1; i <= playerNum; i++) {
+        for (int i = 1; i <= threadCount; i++) {
             ***REMOVED***
             playerList.add(player);
         }
         for (PokerCard.PokerNumEnum numEnum : PokerCard.PokerNumEnum.values()) {
             if (numEnum.equals(PokerCard.PokerNumEnum.RED_JOKER) ||
                     numEnum.equals(PokerCard.PokerNumEnum.BLACK_JOKER)) {
-                PokerCard pokerCard = new PokerCard(PokerCard.ColorEnum.SPADE, numEnum);
+                PokerCard pokerCard = new PokerCard(PokerCard.ColorEnum.SPADE, numEnum, reformCardCode);
                 pokerCardSet.add(pokerCard);
                 continue;
             }
             for (PokerCard.ColorEnum colorEnum : PokerCard.ColorEnum.values()) {
-                PokerCard pokerCard = new PokerCard(colorEnum, numEnum);
-                // set color's number by playWay
-                pokerCard.setColorNum(playWay.getColorEnum(), playWay.getColorNum());
+                PokerCard pokerCard = new PokerCard(colorEnum, numEnum, reformCardCode);
                 pokerCardSet.add(pokerCard);
             }
         }
     }
 
-    public int playing() {
-        int numResult = 0;
-        for (Player player : playerList) {
-            if (null != player.getPokerCard()) {
-                numResult += player.getPokerCard().getNumber().num;
-                //将手里牌清空
-                player.setPokerCard(null);
+
+    public void playing(Integer ruleSum) throws InterruptedException {
+        int roundNum;
+        if (pokerCardSet.size() % threadCount > 0) {
+            roundNum = (pokerCardSet.size() / threadCount) + 1;
+        } else {
+            roundNum = pokerCardSet.size() / threadCount;
+        }
+        for (int j = 0; j < roundNum; j++) {
+            System.out.println("..........round" + (j + 1) + ".......");
+            //reset numResult
+            numResult = 0;
+            for (Player player : playerList) {
+                //use multi-thread
+                Thread thread = new Thread(new Send(player));
+                thread.setName("thread-" + player.getName());
+                thread.start();
+                thread.join();
+            }
+            System.out.println("numResult=" + numResult);
+            if (numResult >= ruleSum) {
+                System.out.println("player win game over!!");
+                break;
             }
         }
-        System.out.println("numResult=" + numResult);
-        return numResult;
     }
+
 
     public class Send implements Runnable {
         private Player player;
-        private PokerCard pokerCard;
 
-        Send(Player player, PokerCard pokerCard) {
+        Send(Player player) {
             this.player = player;
-            this.pokerCard = pokerCard;
         }
+
         @Override
         public void run() {
-            synchronized (player) {
-                if (null == player.getPokerCard() && pokerCard.isSendFlag() == false) {
-                    System.out.println("........thread is running:" + Thread.currentThread().getName());
-                    //send card
-                    player.setPokerCard(pokerCard);
-                    pokerCard.setSendFlag(true);
-                    String format = "player=%s,num=%s";
-                    System.out.println(String.format(format, player.getName(), player.getPokerCard().getNumber().num));
+            if (null == player.getPokerCard()) {
+                Iterator<PokerCard> pokerCardIterator = pokerCardSet.iterator();
+                while (pokerCardIterator.hasNext()) {
+                    PokerCard pokerCard = pokerCardIterator.next();
+                    numResult += pokerCard.getNumber().num;
+                    pokerCardIterator.remove();
+                    String format = "player=%s,num=%s,color=%s,thread=%s";
+                    System.out.println(String.format(format, player.getName(), pokerCard.getNumber().num, pokerCard.getColor().colorStr, Thread.currentThread().getName()));
+                    break;
                 }
             }
         }
-    }
-
-//    public void send() {
-//        for (Player player : playerList) {
-//            for (PokerCard pokerCard : pokerCardSet) {
-//                if (null == player.getPokerCard()) {
-//                    System.out.println("........thread is running:" + Thread.currentThread().getName());
-//                    //send card
-//                    player.setPokerCard(pokerCard);
-//                    //remove
-//                    pokerCardSet.remove(pokerCard);
-//                    break;
-//                }
-//            }
-//        }
-//    }
-
-
-    public List<Player> getPlayerList() {
-        return playerList;
-    }
-
-    public void setPlayerList(List<Player> playerList) {
-        this.playerList = playerList;
-    }
-
-    public Set<PokerCard> getPokerCardSet() {
-        return pokerCardSet;
-    }
-
-    public void setPokerCardSet(Set<PokerCard> pokerCardSet) {
-        this.pokerCardSet = pokerCardSet;
-    }
-
-    public Integer getPlayNum() {
-        return playNum;
-    }
-
-    public void setPlayNum(Integer playNum) {
-        this.playNum = playNum;
     }
 }
