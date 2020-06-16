f093dddf7367ce939a9a8f132f13a429989c58f4
aiden_jia <Aiden_Jia@epam.com>
2020-06-16 11:19:16 +0800

    logback demo

---
+++ springboot-demo/src/main/java/com/jiaz/springboot/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.jiaz.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jia
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
