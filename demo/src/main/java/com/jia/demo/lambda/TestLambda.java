eae07b003b87ae15f80b47b5ae449482a19911ef
Aiden_Jia <Aiden_Jia@epam.com>
2021-04-21 18:27:01 +0800

    lambda

---
+++ demo/src/main/java/com/jia/demo/lambda/TestLambda.java
@@ -0,0 +1,53 @@
+package com.jia.demo.lambda;
+
+public class TestLambda {
+
+    static class FuncClass2 implements FuncInterface{
+        @Override
+        public void funcInter() {
+            System.out.println("I like Lambda1");
+        }
+    }
+
+    public static void main(String[] args) {
+        FuncInterface funcInterface=new FuncClass();
+        funcInterface.funcInter();
+
+        funcInterface=new FuncClass2();
+        funcInterface.funcInter();
+
+        class FuncClass3 implements FuncInterface{
+            @Override
+            public void funcInter() {
+                System.out.println("I like Lambda2");
+            }
+        }
+        funcInterface=new FuncClass3();
+        funcInterface.funcInter();
+
+        //匿名内部类
+        funcInterface=new FuncInterface() {
+            @Override
+            public void funcInter() {
+                System.out.println("I like Lambda3");
+            }
+        };
+        funcInterface.funcInter();
+
+        funcInterface=()->System.out.println("I like Lambda5");
+        funcInterface.funcInter();
+    }
+}
+
+interface FuncInterface {
+    void funcInter();
+}
+
+class FuncClass implements FuncInterface{
+    @Override
+    public void funcInter() {
+        System.out.println("I like Lambda");
+    }
+}
+
+
