6087dc74688381aa4d42e4adb92e1c8471377191
Aiden_Jia <Aiden_Jia@epam.com>
2022-03-02 18:11:39 +0800

    paixu

--- demo/src/main/java/com/jia/demo/paixu/FastSortDemo.java
+++ demo/src/main/java/com/jia/demo/paixu/FastSortDemo.java
@@ -4,35 +4,38 @@ import java.util.Arrays;
 
 public class FastSortDemo {
     public static void main(String[] args) {
-        int [] arr = {65,58,95,10,57,62,13,106,78,23,85};
-        System.out.println("排序前："+ Arrays.toString(arr));
-        quickSort(arr,0,arr.length-1);
-        System.out.println("排序后："+Arrays.toString(arr));
+        int[] arr = {65, 58, 95, 10, 57, 62, 13, 106, 78, 23, 85};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("排序后：" + Arrays.toString(arr));
     }
 
-    public static void quickSort(int [] arr,int left,int right) {
-        int pivot = 0;
-        if(left < right) {
-            pivot = partition(arr,left,right);
-            quickSort(arr,left,pivot-1);
-            quickSort(arr,pivot+1,right);
+    public static void quickSort(int[] arr, int low, int high) {
+        int p, i, j, temp;
+        if (low >= high) {
+            return;
         }
-    }
-
-    private static int partition(int[] arr,int left,int right) {
-        int key = arr[left];
-        while(left < right) {
-            while(left < right && arr[right] >= key) {
-                right--;
+        //p是基准数,约定为每个数组的第一个
+        p = arr[low];
+        i = low;
+        j = high;
+        while (i < j) {
+            //当右边发现大于p的时候停止循环
+            while (arr[j] >= p && i < j) {
+                j--;
             }
-            arr[left] = arr[right];
-            while(left < right && arr[left] <= key) {
-                left++;
+            //当左边发现小于p的时候 停止
+            while (arr[i] <= p && i < j) {
+                i++;
             }
-            arr[right] = arr[left];
+            temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
         }
-        arr[left] = key;
-        return left;
-
+        //此时a[i]一定是小于p的，因为在while循环中已经做了比对，所以将i==j时的值与基准值进行交换
+        arr[low] = arr[i];
+        arr[i] = p;
+        quickSort(arr, low, j - 1);  //对左边快排
+        quickSort(arr, j + 1, high); //对右边快排
     }
 }
--- demo/src/main/java/com/jia/demo/paixu/InsertDemo.java
+++ demo/src/main/java/com/jia/demo/paixu/InsertDemo.java
@@ -9,7 +9,7 @@ public class InsertDemo {
             System.out.print(arr[i] + " ");
         }
         // 直接插入排序
-        insertSort(arr);
+        insertSort2(arr);
         System.out.println();
         System.out.println("排序之后：");
         for (int i = 0; i < arr.length; i++) {
@@ -20,7 +20,7 @@ public class InsertDemo {
     /**
      * 直接插入排序
      */
-    private static void insertSort(int[] arr) {
+    private static void insertSort2(int[] arr) {
         int j; // 已排序列表下标
         int t; // 待排序元素
         for (int i = 1; i < arr.length; i++) {
@@ -29,8 +29,29 @@ public class InsertDemo {
                 for (j = i - 1; j >= 0 && arr[j] > t; j--) {
                     arr[j + 1] = arr[j]; // 从后往前遍历已排序列表，逐个和待排序元素比较，如果已排序元素较大，则将它后移
                 }
-                arr[j + 1] = t; // 将待排序元素插入到正确的位置
+                arr[j+1] = t; // 将待排序元素插入到正确的位置
             }
+        }
+    }
+
+    private static void insertSort(int[] arr){
+        for(int i=1;i<arr.length;i++){
+            int temp =arr[i];
+            int j=i;
+            // 从已经排序的序列最右边开始比较，找到比其小的数
+            while (j>0 && temp<arr[j-1]){
+                arr[j]=arr[j-1];
+                j--;
+            }
+            // 存在比其小的数，则插入
+            if(j!=i){
+                arr[j]=temp;
+            }
+            System.out.println();
+            for (int index = 0; index < arr.length;index++) {
+                System.out.print(arr[index] + " ");
+            }
+            System.out.println();
         }
     }
 
